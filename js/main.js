ff0d5f2faf0dfbe389abdcd745b91381f9d44d1e
PolarizedIons <polarizedions@gmail.com>
2018-08-20 21:41:26 +0200

    Add 404 page

--- js/main.js
+++ js/main.js
@@ -17,17 +17,27 @@ let welcomeMsgs = [
     "Cats > Dogs. Fite me"
 ];
 
+let notfoundMsgs = [
+    "OwO, this isn't what you were looking for, was it?",
+    "Nope, not here. I checked.",
+    "Sorry, but the page you were looking for is in a different castle!",
+];
+
 
 let fields = {
     "welcome": random(welcomeMsgs),
     "age": calcAge(),
     "copyyear": new Date().getFullYear(),
+    "error404": random(notfoundMsgs),
 };
 
 function initFields() {
   for (let key in fields) {
-         let value = fields[key];
-        document.getElementById(key).innerHTML = value;
+        let value = fields[key];
+        let element = document.getElementById(key);
+        if (element !== null) {
+            element.innerHTML = value;
+        }
   }
 }
 
