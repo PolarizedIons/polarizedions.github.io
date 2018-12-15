247f7ee6cd3d0ee576b69733f488752df80db2ef
PolarizedIons <polarizedions@gmail.com>
2018-12-15 18:44:36 +0200

    make sure the dom is loaded here

--- js/misc.js
+++ js/misc.js
@@ -1,8 +1,18 @@
+function initDownArrow() {
+    const elDownArrow = document.getElementById("more-arrow");
+    const elSecondSection = document.getElementsByTagName("section")[1];
+    elDownArrow.onclick = () => {
+        window.scrollTo(0, elSecondSection.offsetTop);
+    };
+    elDownArrow.style.cursor = 'pointer';
+}
 
-// Add functionality to down arrow
-const elDownArrow = document.getElementById("more-arrow");
-const elSecondSection = document.getElementsByTagName("section")[1];
-elDownArrow.onclick = () => {
-    window.scrollTo(0, elSecondSection.offsetTop);
-};
-elDownArrow.style.cursor = 'pointer';
+function init() {
+    initDownArrow();
+}
+
+if (document.readyState === "loading") {  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", init);
+} else {  // `DOMContentLoaded` has already fired
+    init();
+}
