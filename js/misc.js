dc5d830bfdf6648359e11a2278ede00c18776915
PolarizedIons <polarizedions@gmail.com>
2018-12-16 00:44:00 +0200

    scrollspy

--- js/misc.js
+++ js/misc.js
@@ -1,18 +1,44 @@
-function initDownArrow() {
-    const elDownArrow = document.getElementById("more-arrow");
-    const elSecondSection = document.getElementsByTagName("section")[1];
+function downArrow() {
+    const elDownArrow = document.querySelector("#more-arrow");
+    const elSecondSection = document.querySelector("section")[1];
     elDownArrow.onclick = () => {
         window.scrollTo(0, elSecondSection.offsetTop);
     };
     elDownArrow.style.cursor = 'pointer';
 }
 
+function scrollSpy() {
+    // init
+    document.querySelector("nav a").setAttribute('class', 'active');
+    const headerHeight = document.querySelector("header").style.height;
+    const sections = {};
+    for (section of document.querySelectorAll("section")) {
+        sections[section.id] = section.offsetTop;
+    };
+
+    const update = () => {
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        for (id in sections) {
+            if (sections[id] <= scrollPosition + headerHeight) {
+                document.querySelector('nav a.active').setAttribute('class', ' ');
+                document.querySelector('nav a[href*=' + id + ']').setAttribute('class', 'active');
+                
+            }
+        }
+    };
+
+    window.onscroll = update;
+    update();
+}
+
 function init() {
-    initDownArrow();
+    downArrow();
+    scrollSpy();
 }
 
 if (document.readyState === "loading") {  // Loading hasn't finished yet
-  document.addEventListener("DOMContentLoaded", init);
+    document.addEventListener("DOMContentLoaded", init);
 } else {  // `DOMContentLoaded` has already fired
     init();
 }
